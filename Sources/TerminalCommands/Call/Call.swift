29babaaf860486e62953cc3eecdb56e4668b730b
Christophe Bronner <christophe.bronner@videotron.ca>
2020-03-01 23:31:36 -0500

    Allowed getting optional argument

--- Sources/TerminalCommands/Call/Call.swift
+++ Sources/TerminalCommands/Call/Call.swift
@@ -8,6 +8,11 @@ extension Command {
         
         private var args: [Substring]
         
+        //MARK: Computed Properties
+        
+        /// Wether the call has ran out of arguments
+        var isAtEnd: Bool { args.isEmpty }
+        
         //MARK: Initialization
         
         init(_ args: [Substring]) {
@@ -18,12 +23,12 @@ extension Command {
         
         /// Makes sure there are no arguments left in the call
         public func assertEmpty() throws {
-            guard args.isEmpty else { throw Errors.expectedNoArguments }
+            guard isAtEnd else { throw Errors.expectedNoArguments }
         }
         
         /// Makes sure there are arguments left in the call
         public func assertNotEmpty() throws {
-            guard !args.isEmpty else { throw Errors.expectedArgument }
+            guard !isAtEnd else { throw Errors.expectedArgument }
         }
 
         /// Makes sure there are exactly `n` arguments left in the call
@@ -40,18 +45,21 @@ extension Command {
         
         //MARK: Pop
         
+        /// Gets a single argument from the call
         public func single(final f: Bool = true) throws -> Argument {
             if f { try assert(exactly: 1) }
             else { try assert(minimum: 1) }
             return try next()
         }
 
+        /// Gets two arguments from the call
         func double(final f: Bool = true) throws -> (Argument, Argument) {
             if f { try assert(exactly: 2) }
             else { try assert(minimum: 2) }
             return (try next(), try next())
         }
 
+        /// Gets three arguments from the call
         func triple(final f: Bool = true) throws -> (Argument, Argument, Argument) {
             if f { try assert(exactly: 3) }
             else { try assert(minimum: 3) }
@@ -60,9 +68,16 @@ extension Command {
         
         //MARK: Utilities
 
+        /// Gets the next argument of the call
         public func next() throws -> Argument {
             try assertNotEmpty()
             return Argument(args.removeFirst())
+        }
+        
+        /// Attempts to get the next argument of the call
+        @discardableResult
+        public func next() -> Argument? {
+            try? next()
         }
         
     }
