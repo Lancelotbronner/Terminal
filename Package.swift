28af3ecce2a12d994ef1540d6d079fcf99eecb18
Christophe Bronner <christophe.bronner@videotron.ca>
2020-03-01 20:56:32 -0500

    Added terminal commands

--- Package.swift
+++ Package.swift
@@ -8,9 +8,11 @@ let package = Package(
     products: [
         .library(name: "Terminal", targets: ["Terminal"]),
         .library(name: "TerminalUI", targets: ["Terminal", "TerminalUI"]),
+        .library(name: "TerminalCommands", targets: ["Terminal", "TerminalCommands"]),
     ],
     targets: [
         .target(name: "Terminal"),
         .target(name: "TerminalUI", dependencies: ["Terminal"]),
+        .target(name: "TerminalCommands", dependencies: ["Terminal"]),
     ]
 )
---
+++ Sources/TerminalCommands/States/State.swift
@@ -0,0 +1,34 @@
+
+//MARK: - State Protocol
+
+public protocol State {
+    var controls: [CommandGroup] { get }
+    var name: String { get }
+    
+    init()
+    
+    func onEnter()
+    func onExit()
+}
+
+extension State {
+    
+    //MARK: Computed Properties
+    
+    public var name: String { String(describing: Self.self) }
+    
+    //MARK: Default Implementation
+    
+    public func onEnter() { }
+    public func onExit() { }
+    
+}
+
+//MARK: - Empty
+
+public struct EmptyState: State {
+    public var name: String { StateMachine._emptyStateName }
+    public var controls: [CommandGroup] = []
+    
+    public init() { }
+}
